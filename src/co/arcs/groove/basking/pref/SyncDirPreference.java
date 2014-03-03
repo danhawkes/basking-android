882d0d62460ed3ba8af5a6a0a368628424bd2974
danhawkes <dan@danhawkes.co.uk>
2014-03-03 22:44:57 +0000

    Integrate file selector library.

--- src/co/arcs/groove/basking/pref/SyncDirPreference.java
+++ src/co/arcs/groove/basking/pref/SyncDirPreference.java
@@ -1,11 +1,18 @@
 package co.arcs.groove.basking.pref;
 
+import java.io.File;
+
 import android.content.Context;
-import android.preference.EditTextPreference;
+import android.preference.Preference;
 import android.preference.PreferenceManager;
 import android.util.AttributeSet;
 
-public class SyncDirPreference extends EditTextPreference {
+public class SyncDirPreference extends Preference {
+
+	interface Listener {
+
+		void onClickSyncDirPreference(SyncDirPreference preference);
+	}
 
 	public SyncDirPreference(Context context) {
 		super(context);
@@ -20,11 +27,29 @@ public class SyncDirPreference extends EditTextPreference {
 	}
 
 	@Override
+	protected void onClick() {
+		super.onClick();
+		Context context = getContext();
+		if (context instanceof Listener) {
+			((Listener) context).onClickSyncDirPreference(this);
+		}
+	}
+
+	@Override
 	protected void onAttachedToHierarchy(PreferenceManager preferenceManager) {
 		super.onAttachedToHierarchy(preferenceManager);
 		String path = getPersistedString(null);
 		if (path != null) {
 			setSummary(path);
 		}
+	}
+
+	/**
+	 * Callback for when the user has selected a new sync directory.
+	 */
+	public void onDirectorySelected(File directory) {
+		String path = directory.getAbsolutePath();
+		persistString(path);
+		setSummary(path);
 	}
 }
